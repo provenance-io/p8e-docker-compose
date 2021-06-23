05200f67e1648b7485a0e676dc99ee669a5e000c
Pierce Trey <ptrey@figure.com>
2021-06-23 14:51:03 -0600

    simplify node config volume mounts
    - copy appropriate files for node to config directory based on prefix in custom_entrypoint.sh setup (shared/node#) (configMaps can't have a folder structure to them unfortunately)

--- kubernetes/local/provenance/config/shared/custom_entrypoint.sh
+++ kubernetes/local/provenance/config/shared/custom_entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+echo "Starting node$ID"
 
 if [ ! -f "/home/provenance/data/priv_validator_state.json" ]; then
-    echo "Initializing /home/provenance/data/priv_validator_state.json"
+    echo "Initializing validator configuration for node$ID"
+
+    for file in /home/provenance/seed/shared-*; do
+        cp "$file" "/home/provenance/config/${file##*/shared-}" || exit $?
+    done;
+
+    for file in /home/provenance/seed/node$ID-*; do
+        cp "$file" "/home/provenance/config/${file##*/node$ID-}" || exit $?
+    done;
 
     cat > /home/provenance/data/priv_validator_state.json <<EOF
 {
@@ -10,6 +19,11 @@ if [ ! -f "/home/provenance/data/priv_validator_state.json" ]; then
     "step": 0
 }
 EOF
+
+    code=$?
+    if [ $code != 0 ]; then
+        exit $code
+    fi
 fi
 
 provenanced --testnet --home /home/provenance start

53f82d5dc339eda5e846751f4da24f11f09b5fdc
Pierce Trey <ptrey@figure.com>
2021-06-24 11:48:03 -0600

    update postgres version (hopefully has arm64 support)

--- kubernetes/local/p8e/init_keys.sh
+++ kubernetes/local/p8e/init_keys.sh
@@ -2,4 +2,4 @@
 
 echo "creating p8e test affiliate keys"
 
-psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -f /p8e/keys.sql
+psql -f /p8e/keys.sql postgres://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME
